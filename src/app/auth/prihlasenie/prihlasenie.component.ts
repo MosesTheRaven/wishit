3edd2364e96915e49dfc8a59cba74e3efeeb7a0f
davidlazar <dajo.lazar@gmail.com>
2017-10-22 23:59:23 +0200

    installed bootstrap and jquery
    one error:
    Uncaught Error: Bootstrap's JavaScript requires jQuery

---
+++ src/app/auth/prihlasenie/prihlasenie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prihlasenie',
+  templateUrl: './prihlasenie.component.html',
+  styleUrls: ['./prihlasenie.component.css']
+})
+export class PrihlasenieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
