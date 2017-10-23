e8e4489f9c9672fd7817aebf2aa84ed59e89e2a3
davidlazar <dajo.lazar@gmail.com>
2017-10-23 23:24:14 +0200

    added firebase // npm install firebase --save
    connected wishit to firebase database - check app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+
+  ngOnInit(){
+    const config = {
+      apiKey: "AIzaSyC2E2mZW49OXg2l3p-O7W1fhonGlhmzwSI",
+      authDomain: "wishit-d11d7.firebaseapp.com",
+      databaseURL: "https://wishit-d11d7.firebaseio.com",
+      projectId: "wishit-d11d7",
+      storageBucket: "wishit-d11d7.appspot.com",
+      messagingSenderId: "211506530222"
+    };
+    firebase.initializeApp(config);
+  }
+
 }
