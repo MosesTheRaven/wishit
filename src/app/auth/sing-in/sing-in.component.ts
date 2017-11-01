555e2dc794dade5938aad9b57d8495e2e62908a0
davidlazar <dajo.lazar@gmail.com>
2017-11-01 22:15:49 +0100

    signing in now works - it creates a user with email and password, also performs some controls (pw strenght, email format) - sends a verification email - adds created user to the db into users/
    // IJ shows error on .then in sing-in.com....ts - ignore it

--- src/app/auth/sing-in/sing-in.component.ts
+++ src/app/auth/sing-in/sing-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import * as firebase from 'firebase';
+import {register} from "ts-node/dist";
 
 @Component({
   selector: 'app-sing-in',
@@ -10,6 +13,30 @@ export class SingInComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form : NgForm){
+    const nickname = form.value.nickname;
+    const email = form.value.email;
+    const password = form.value.password;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(function (userData) {
+        userData.sendEmailVerification();
+
+        return firebase.database().ref('users/' + userData.uid).set({
+          email: email,
+          uid: userData.uid,
+          registrationDate: new Date().toString(),
+          nickname: nickname
+        }).then(function() {
+          //to be resolved - doesnt break the building process
+            firebase.auth().signOut();
+          })
+
+      }).catch(function (error) {
+        console.log(error);
+      });
   }
 
 }
